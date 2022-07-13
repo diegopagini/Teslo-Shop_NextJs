97695043049e1bf3189aeb4767bbb9e033ea7699
Diego Pagini <diego.pagini@neoris.com>
2022-07-13 10:23:04 +0200

    history

---
+++ pages/orders/[id].tsx
@@ -0,0 +1,85 @@
+/** @format */
+import { CreditScoreOutlined } from '@mui/icons-material';
+import { Box, Card, CardContent, Chip, Divider, Grid, Link, Typography } from '@mui/material';
+import NextLink from 'next/link';
+
+import { CardList, OrderSummary } from '../../components/cart';
+import { ShopLayout } from '../../components/layouts';
+
+const OrderPage = () => {
+	return (
+		<ShopLayout
+			title='Resumen de Orden 123123'
+			pageDescription='Resumen de la Orden'>
+			<Typography variant='h1' component='h1'>
+				Orden: ABC123
+			</Typography>
+
+			{/* <Chip
+				sx={{ my: 2 }}
+				label='Pendiente de pago'
+				variant='outlined'
+				color='error'
+				icon={<CreditCardOutlined />}
+			/> */}
+			<Chip
+				sx={{ my: 2 }}
+				label='Orden Pagada'
+				variant='outlined'
+				color='success'
+				icon={<CreditScoreOutlined />}
+			/>
+
+			<Grid container>
+				<Grid item xs={12} sm={7}>
+					<CardList />
+				</Grid>
+				<Grid item xs={12} sm={5}>
+					<Card className='summary-card'>
+						<CardContent>
+							<Typography variant='h2'>Resumen (3 productos)</Typography>
+							<Divider sx={{ my: 1 }} />
+
+							<Box display='flex' justifyContent='space-between'>
+								<Typography variant='subtitle1'>
+									Dirección de Entrega
+								</Typography>
+								<NextLink href='/checkout/address' passHref>
+									<Link underline='always'>Editar</Link>
+								</NextLink>
+							</Box>
+
+							<Typography>Diego Pagini</Typography>
+							<Typography>Algun Lugar</Typography>
+							<Typography>29640</Typography>
+							<Typography>España</Typography>
+
+							<Divider sx={{ my: 2 }} />
+
+							<Box display='flex' justifyContent='end'>
+								<NextLink href='/cart' passHref>
+									<Link underline='always'>Editar</Link>
+								</NextLink>
+							</Box>
+							<OrderSummary />
+
+							<Box sx={{ mt: 3 }}>
+								{/* TODO */}
+								<h1>Pagar</h1>
+								<Chip
+									sx={{ my: 2 }}
+									label='Orden Pagada'
+									variant='outlined'
+									color='success'
+									icon={<CreditScoreOutlined />}
+								/>
+							</Box>
+						</CardContent>
+					</Card>
+				</Grid>
+			</Grid>
+		</ShopLayout>
+	);
+};
+
+export default OrderPage;
---
+++ pages/orders/history.tsx
@@ -0,0 +1,98 @@
+/** @format */
+import { Chip, Grid, Link, Typography } from '@mui/material';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import NextLink from 'next/link';
+
+import { ShopLayout } from '../../components/layouts';
+
+const columns: GridColDef[] = [
+	{
+		field: 'id',
+		headerName: 'ID',
+		width: 100,
+	},
+	{
+		field: 'fullname',
+		headerName: 'Nombre Completo',
+		width: 300,
+	},
+	{
+		field: 'paid',
+		headerName: 'Pagada',
+		description: 'Muestra información si está pagada o no.',
+		width: 200,
+		renderCell: (params: GridValueGetterParams) => {
+			return params.row.paid ? (
+				<Chip color='success' label='Pagada' variant='outlined' />
+			) : (
+				<Chip color='error' label='No Pagada' variant='outlined' />
+			);
+		},
+	},
+	{
+		field: 'order',
+		headerName: 'Ver Orden',
+		width: 200,
+		sortable: false,
+		renderCell: (params: GridValueGetterParams) => {
+			return (
+				<NextLink href={`/orders/${params.row.id}`} passHref>
+					<Link underline='always'>Ver orden</Link>
+				</NextLink>
+			);
+		},
+	},
+];
+
+const rows = [
+	{
+		id: 1,
+		paid: true,
+		fullname: 'Diego Pagini',
+	},
+	{
+		id: 2,
+		paid: false,
+		fullname: 'Diego Pagini',
+	},
+	{
+		id: 3,
+		paid: true,
+		fullname: 'Diego Pagini',
+	},
+	{
+		id: 4,
+		paid: true,
+		fullname: 'Diego Pagini',
+	},
+	{
+		id: 5,
+		paid: false,
+		fullname: 'Diego Pagini',
+	},
+];
+
+const HistoryPage = () => {
+	return (
+		<ShopLayout
+			title='Historial de ordenes'
+			pageDescription='Historial de ordenes del cliente'>
+			<Typography variant='h1' component='h1'>
+				Historial de ordenes
+			</Typography>
+
+			<Grid container>
+				<Grid item xs={12} sx={{ height: 650, width: '100%' }}>
+					<DataGrid
+						rows={rows}
+						columns={columns}
+						pageSize={10}
+						rowsPerPageOptions={[10]}
+					/>
+				</Grid>
+			</Grid>
+		</ShopLayout>
+	);
+};
+
+export default HistoryPage;
