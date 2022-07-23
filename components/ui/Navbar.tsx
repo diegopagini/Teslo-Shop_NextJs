5da17785540574828792bb666330d8c0c4281c31
Diego Pagini <diegopaginidev@gmail.com>
2022-07-23 14:17:37 +0200

    cart counter

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -6,10 +6,13 @@ import { useRouter } from 'next/router';
 import { useContext, useState } from 'react';
 
 import { UiContext } from '../../context';
+import { CartContext } from '../../context/cart/CartContext';
 
 export const Navbar = () => {
 	const { asPath, push } = useRouter();
 	const { toggleSideMenu } = useContext(UiContext);
+	const { numberOfItems } = useContext(CartContext);
+
 	const [searchTerm, setSearchTerm] = useState('');
 	const [isSearchVisible, setIsSearchVisible] = useState(false);
 
@@ -94,7 +97,7 @@ export const Navbar = () => {
 				<NextLink href='/cart' passHref>
 					<Link>
 						<IconButton>
-							<Badge badgeContent={2} color='secondary'>
+							<Badge badgeContent={numberOfItems > 9 ? '+9' : numberOfItems} color='secondary'>
 								<ShoppingCartOutlined />
 							</Badge>
 						</IconButton>
