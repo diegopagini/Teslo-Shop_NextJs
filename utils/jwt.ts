48dd061a6bfdee4eef54006a30173f930561cffd
Diego Pagini <diegopaginidev@gmail.com>
2022-07-23 23:29:49 +0200

    validate token

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -7,7 +7,6 @@ import jwt from 'jsonwebtoken';
  * @param {string} email
  * @returns string
  */
-
 export const signToken = (_id: string, email: string) => {
 	if (!process.env.JWT_SECRET_SEED) {
 		throw new Error('No hay semilla de JWT - Revisar variables de entorno');
@@ -23,4 +22,33 @@ export const signToken = (_id: string, email: string) => {
 			expiresIn: '30d',
 		}
 	);
+};
+
+/**
+ * Función para validar Tokens
+ * @param {string} token
+ * @returns Promise<string>
+ */
+export const isValidToken = (token: string): Promise<string> => {
+	if (!process.env.JWT_SECRET_SEED) {
+		throw new Error('No hay semilla de JWT - Revisar variables de entorno');
+	}
+
+	return new Promise((resolve, reject) => {
+		/**
+		 * Recuperar los datos del JWT
+		 */
+
+		try {
+			jwt.verify(token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
+				if (err) return reject('JWT no es válido');
+
+				const { _id } = payload as { _id: string };
+
+				resolve(_id);
+			});
+		} catch (error) {
+			reject('JWT no es válido');
+		}
+	});
 };
