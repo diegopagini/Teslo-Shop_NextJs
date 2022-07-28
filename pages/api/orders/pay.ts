cd2ac77984b7f1c8cb58c54fe169f4cb5bf8aa9e
Diego Pagini <diego.pagini@neoris.com>
2022-07-28 20:30:58 +0200

    Paypal token

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -1,4 +1,5 @@
 /** @format */
+import axios from 'axios';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 
@@ -16,6 +17,39 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 	}
 }
 
+const getPaypalBearerToken = async (): Promise<string | null> => {
+	const PAYPAL_CLIENT = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '';
+	const PAYPAL_SECRET = process.env.PAYPAL_SECRET || '';
+
+	const base64Token = Buffer.from(`${PAYPAL_CLIENT}:${PAYPAL_SECRET}`, 'utf-8').toString('base64');
+	const body = new URLSearchParams('grant_type=client_credentials');
+
+	try {
+		const { data } = await axios.post(process.env.PAYPAL_OAUTH_URL || '', body, {
+			headers: {
+				Authorization: `Basic ${base64Token}`,
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+		});
+
+		return data.access_token;
+	} catch (error) {
+		if (axios.isAxiosError(error)) {
+			console.log(error.response?.data);
+		} else {
+			console.log(error);
+		}
+
+		return null;
+	}
+};
+
 const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-	return res.status(200).json({ message: 'Orden pagada' });
+	const paypalBearerToken = await getPaypalBearerToken();
+
+	if (!paypalBearerToken) {
+		return res.status(400).json({ message: 'No se pudo generar token de Paypal' });
+	}
+
+	return res.status(200).json({ message: paypalBearerToken });
 };
